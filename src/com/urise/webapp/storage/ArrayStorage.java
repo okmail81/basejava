a00d83a7896fe832fb10f234ebebb7c2bca76f4d
okmail81 <okmail81@mail.ru>
2022-04-02 16:47:00 +0300

    HW3

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,14 +4,17 @@ import com.urise.webapp.model.Resume;
 
 public class ArrayStorage extends AbstractArrayStorage {
 
+    @Override
     protected void saveResume(int resumeIndex, Resume r) {
         storage[storageSize] = r;
     }
 
+    @Override
     protected void deleteResume(int resumeIndex) {
         storage[resumeIndex] = storage[storageSize - 1];
     }
 
+    @Override
     protected int findIndex(String uuid, boolean isShowMessage) {
         for (int i = 0; i < storageSize; i++) {
             if (uuid.equals(storage[i].getUuid())) {
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -6,18 +6,21 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
+    @Override
     protected void saveResume(int resumeIndex, Resume r) {
         int insertIndex = -resumeIndex - 1;
         System.arraycopy(storage, insertIndex, storage, insertIndex + 1, storageSize - insertIndex);
         storage[insertIndex] = r;
     }
 
+    @Override
     protected void deleteResume(int resumeIndex) {
         if (storageSize - 1 - resumeIndex >= 0) {
             System.arraycopy(storage, resumeIndex + 1, storage, resumeIndex, storageSize - 1 - resumeIndex);
         }
     }
 
+    @Override
     protected int findIndex(String uuid, boolean isShowMessage) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
