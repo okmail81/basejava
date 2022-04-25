52236d56d5af681c938e186be89cfda7d5332c48
okmail81 <okmail81@mail.ru>
2022-04-25 19:02:24 +0300

    HW7

--- src/com/urise/webapp/TestSingleton.java
+++ src/com/urise/webapp/TestSingleton.java
@@ -5,7 +5,7 @@ import com.urise.webapp.model.SectionType;
 public class TestSingleton {
 
     public static void main(String[] args) {
-         for (SectionType type : SectionType.values()) {
+        for (SectionType type : SectionType.values()) {
             System.out.println(type.getTitle());
         }
     }
