b5475064ca9e7f6c3cca195063acffea45097a78
okmail81 <okmail81@mail.ru>
2022-05-03 18:01:59 +0300

    HW7

--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -2,10 +2,17 @@ package com.urise.webapp;
 
 import com.urise.webapp.model.*;
 
-import java.time.YearMonth;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResumeTestData {
     public static void main(String[] args) {
+        List<String> achievements = new ArrayList<>();
+        List<String> qualifications = new ArrayList<>();
+        List<Organization> organizations = new ArrayList<>();
+        List<Organization> educations = new ArrayList<>();
+
         Resume resume = new Resume("Григорий Кислин");
 
         //контакты
@@ -14,67 +21,67 @@ public class ResumeTestData {
         resume.setContactInformation(ContactType.EMAIL, "gkislin@yandex.ru");
 
         //Личные качества
-        StringType personal = new StringType();
-        personal.setInformation("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры.");
+        TextSection personal = new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры.");
         resume.setSection(SectionType.PERSONAL, personal);
 
         //Позиция
-        StringType objective = new StringType();
-        objective.setInformation("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
+        TextSection objective = new TextSection("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
         resume.setSection(SectionType.OBJECTIVE, objective);
 
         //Достижения
-        BulletedListSection achievement = new BulletedListSection();
-        achievement.setAchievement("Организация команды и успешная реализация Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + Vaadin проект для комплексных DIY смет");
-        achievement.setAchievement("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 3500 выпускников");
-        achievement.setAchievement("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk. ");
+        achievements.add("Организация команды и успешная реализация Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + Vaadin проект для комплексных DIY смет");
+        achievements.add("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 3500 выпускников");
+        achievements.add("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk. ");
+        BulletedListSection achievement = new BulletedListSection(achievements);
         resume.setSection(SectionType.ACHIEVEMENT, achievement);
 
         //Квалификация
-        BulletedListSection qualifications = new BulletedListSection();
-        qualifications.setAchievement("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
-        qualifications.setAchievement("Version control: Subversion, Git, Mercury, ClearCase, Perforce ");
-        qualifications.setAchievement("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB");
-        resume.setSection(SectionType.QUALIFICATIONS, qualifications);
+        qualifications.add("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
+        qualifications.add("Version control: Subversion, Git, Mercury, ClearCase, Perforce ");
+        qualifications.add("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB");
+        BulletedListSection qualification = new BulletedListSection(qualifications);
+        resume.setSection(SectionType.QUALIFICATIONS, qualification);
 
         //Опыт работы
-        ExperienceSection experienceSection = new ExperienceSection();
         String company = "Java Online Projects";
-        YearMonth startDate = YearMonth.of(2013, 10);
-        YearMonth endDate = YearMonth.of(2013, 10);
+        String url = "http://javaops.ru/";
+        LocalDate startDate = LocalDate.of(2013, 10, 1);
+        LocalDate endDate = LocalDate.of(2022, 5, 1);
         String position = "Автор проекта";
         String description = "Создание, организация и проведение Java онлайн проектов и стажировок.";
-        Experience experience = new Experience(company, startDate, endDate, position, description);
-        experienceSection.setAchievement(experience);
+        Organization organization = new Organization(company, url, startDate, endDate, position, description);
+        organizations.add(organization);
 
         company = "Wrike";
-        startDate = YearMonth.of(2014, 10);
-        endDate = YearMonth.of(2016, 1);
+        url = "https://www.wrike.com/";
+        startDate = LocalDate.of(2014, 10, 1);
+        endDate = LocalDate.of(2016, 1, 1);
         position = "Старший разработчик (backend)";
         description = "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.";
-        experience = new Experience(company, startDate, endDate, position, description);
-        experienceSection.setAchievement(experience);
+        organization = new Organization(company, url, startDate, endDate, position, description);
+        organizations.add(organization);
 
-        resume.setSection(SectionType.EXPERIENCE, experienceSection);
+        OrganizationSection organizationSection = new OrganizationSection(organizations);
+        resume.setSection(SectionType.EXPERIENCE, organizationSection);
 
         //Образование
-        position = null;
-
-        ExperienceSection educationSection = new ExperienceSection();
         company = "Coursera";
-        startDate = YearMonth.of(2013, 3);
-        endDate = YearMonth.of(2013, 5);
-        description = "Functional Programming Principles in Scala' by Martin Odersky";
-        experience = new Experience(company, startDate, endDate, position, description);
-        educationSection.setAchievement(experience);
+        url = "https://www.coursera.org/course/progfun";
+        startDate = LocalDate.of(2013, 3, 1);
+        endDate = LocalDate.of(2013, 5, 1);
+        position = "Functional Programming Principles in Scala' by Martin Odersky";
+        Organization education = new Organization(company, url, startDate, endDate, position);
+        educations.add(education);
 
-        company = "Java Online Projects";
-        startDate = YearMonth.of(2011, 3);
-        endDate = YearMonth.of(2011, 4);
-        description = "Курс 'Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.'";
-        experience = new Experience(company, startDate, endDate, position, description);
-        educationSection.setAchievement(experience);
+        company = "Luxoft";
+        url = "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366";
+        startDate = LocalDate.of(2011, 3, 1);
+        endDate = LocalDate.of(2011, 4, 1);
+        position = "Курс 'Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.'";
+        education = new Organization(company, url, startDate, endDate, position);
+        educations.add(education);
 
+        OrganizationSection educationSection = new OrganizationSection(educations);
         resume.setSection(SectionType.EDUCATION, educationSection);
 
         for (ContactType type : ContactType.values()) {
--- src/com/urise/webapp/model/BulletedListSection.java
+++ src/com/urise/webapp/model/BulletedListSection.java
@@ -1,17 +1,37 @@
 package com.urise.webapp.model;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BulletedListSection extends AbstractSection {
-    private final List<String> list = new ArrayList<>();
+    private final List<String> items;
 
-    public void setAchievement(String achievement) {
-        list.add(achievement);
+    public BulletedListSection(List<String> items) {
+        Objects.requireNonNull(items, "items must not be null");
+        this.items = items;
+    }
+
+    public List<String> getItems() {
+        return items;
     }
 
     @Override
     public String toString() {
-        return String.join("\n", list);
+        return String.join("\n", items);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BulletedListSection that = (BulletedListSection) o;
+
+        return items.equals(that.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return items.hashCode();
     }
 }
